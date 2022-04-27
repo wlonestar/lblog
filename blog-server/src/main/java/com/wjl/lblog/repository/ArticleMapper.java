0075ba2b3fa82bdf26cdfbd528d5d926039df9de
wjl-lab <wlonestar@163.com>
2022-04-27 17:02:19 +0800

    fix: delete cos api

--- blog-server/src/main/java/com/wjl/lblog/repository/ArticleMapper.java
+++ blog-server/src/main/java/com/wjl/lblog/repository/ArticleMapper.java
@@ -19,49 +19,108 @@ import java.util.List;
 @Repository
 public interface ArticleMapper extends BaseMapper<Article> {
 
-    @ResultType(ArticleDetailVo.class)
-    @Select("select a.id, a.create_time, a.modify_time, a.title, a.summary, a.image, a.content, c.name " +
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "createTime", column = "create_time"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "summary", column = "summary"),
+            @Result(property = "image", column = "image"),
+            @Result(property = "content", column = "content"),
+            @Result(property = "category", column = "name"),
+    })
+    @Select("select a.id, a.create_time, a.title, a.summary, a.image, a.content, c.name " +
             "from article as a, category as c " +
             "where a.category_id = c.id order by a.create_time DESC")
     IPage<ArticleDetailVo> selectDetailByPage(Page<?> page);
 
-    @ResultType(ArticleDetailVo.class)
-    @Select("select a.id, a.create_time, a.modify_time, a.title, a.summary, a.image, a.content, c.name " +
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "createTime", column = "create_time"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "summary", column = "summary"),
+            @Result(property = "image", column = "image"),
+            @Result(property = "content", column = "content"),
+            @Result(property = "category", column = "name"),
+    })
+    @Select("select a.id, a.create_time, a.title, a.summary, a.image, a.content, c.name " +
             "from article as a, category as c " +
             "where a.category_id = c.id order by a.create_time DESC")
     List<ArticleDetailVo> selectDetailAll();
 
-    @ResultType(ArticleDetailVo.class)
-    @Select("select a.id, a.create_time, a.modify_time, a.title, a.summary, a.image, a.content, c.name " +
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "createTime", column = "create_time"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "summary", column = "summary"),
+            @Result(property = "image", column = "image"),
+            @Result(property = "content", column = "content"),
+            @Result(property = "category", column = "name"),
+    })
+    @Select("select a.id, a.create_time, a.title, a.summary, a.image, a.content, c.name " +
             "from article as a, category as c " +
             "where a.category_id = c.id and a.id = #{id}")
     ArticleDetailVo selectDetailById(@Param("id") Long id);
 
-    @ResultType(ArticleSummaryVo.class)
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "createTime", column = "create_time"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "summary", column = "summary"),
+            @Result(property = "image", column = "image"),
+            @Result(property = "category", column = "name"),
+    })
     @Select("select a.id, a.create_time, a.title, a.summary, a.image, c.name " +
             "from article as a, category as c " +
             "where a.category_id = c.id order by a.create_time desc")
     IPage<ArticleSummaryVo> selectSummaryByPage(Page<?> page);
 
-    @ResultType(ArticleSummaryVo.class)
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "createTime", column = "create_time"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "summary", column = "summary"),
+            @Result(property = "image", column = "image"),
+            @Result(property = "category", column = "name"),
+    })
     @Select("select a.id, a.create_time, a.title, a.summary, a.image, c.name " +
             "from article as a, category as c " +
             "where a.category_id = c.id and c.id = #{cid} order by a.create_time desc")
     IPage<ArticleSummaryVo> selectSummaryByPageAndCid(@Param("cid") Long cid, Page<?> page);
 
-    @ResultType(ArticleSummaryVo.class)
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "createTime", column = "create_time"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "summary", column = "summary"),
+            @Result(property = "image", column = "image"),
+            @Result(property = "category", column = "name"),
+    })
     @Select("select a.id, a.create_time, a.title, a.summary, a.image, c.name " +
             "from article as a, category as c " +
             "where a.category_id = c.id order by a.create_time desc")
     List<ArticleSummaryVo> selectSummaryAll();
 
-    @ResultType(ArticleSummaryVo.class)
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "createTime", column = "create_time"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "summary", column = "summary"),
+            @Result(property = "image", column = "image"),
+            @Result(property = "category", column = "name"),
+    })
     @Select("select a.id, a.create_time, a.title, a.summary, a.image, c.name " +
             "from article as a, category as c " +
             "where a.category_id = c.id and c.id = #{cid} order by a.create_time desc")
     List<ArticleSummaryVo> selectSummaryAllByCid(@Param("cid") Long cid);
 
-    @ResultType(ArticleSummaryVo.class)
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "createTime", column = "create_time"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "summary", column = "summary"),
+            @Result(property = "image", column = "image"),
+            @Result(property = "category", column = "name"),
+    })
     @Select("select a.id, a.create_time, a.title, a.summary, a.image, c.name " +
             "from article as a, category as c " +
             "where a.category_id = c.id and a.id = #{id}")
